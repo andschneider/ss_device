c51bf9e307934e28cf1bb70b03a77f8de54c094f
Andrew Schneider <aschne93@gmail.com>
2020-05-24 23:02:14 -0700

    MQTT arduino messager
    Sends a basic message with sensor data every two minutes.

--- soilsensor_basic/soilsensor_basic.ino
+++ soilsensor_basic/soilsensor_basic.ino
@@ -1,11 +1,44 @@
+#include <ArduinoMqttClient.h>
 #include "Adafruit_seesaw.h"
-
 Adafruit_seesaw ss;
+
+#include "wifi.h"
+
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "192.168.1.189";
+int        port     = 1883;
+const char topic[]  = "arduino/test";
+
+// sensor settings
+const int capLen = 60;
+uint16_t capVals[capLen];
+char* sensorId = "sensor1";
+const int loopDelay = 2000;  // milliseconds
+
+void connectMqtt(char* clientId) {
+  mqttClient.setId(clientId);
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+}
 
 void setup() {
   Serial.begin(115200);
+  delay(100);
 
-  Serial.println("seesaw Soil Sensor example!");
+  connectWifi();
+  connectMqtt(sensorId);
 
   if (!ss.begin(0x36)) {
     Serial.println("ERROR! seesaw not found");
@@ -16,35 +49,47 @@ void setup() {
   }
 }
 
-const int capLen = 60;
-uint16_t capVals[capLen];
-int count = 0;
-
 uint16_t capAvg(uint16_t vals[]) {
   uint16_t sum = 0;
   for (int i = 0; i < capLen; i++) {
     sum += vals[i];
-    Serial.print("avg i: "); Serial.println(i);
   }
   uint16_t avg = sum / capLen;
   return avg;
 }
 
+void sendMessage(uint16_t avg, float temp) {
+  // send a json like message to the broker
+  // {moisture: 0, temperature: 0.0, sid: "1"}
+  mqttClient.beginMessage(topic);
+  mqttClient.print("{moisture: ");
+  mqttClient.print(avg);
+  mqttClient.print(", temperature: ");
+  mqttClient.print(temp);
+  mqttClient.print(", sid: ");
+  mqttClient.print(sensorId);
+  mqttClient.print("}");
+  mqttClient.endMessage();
+  Serial.println("message sent");
+}
+
+int count = 0;
+
 void loop() {
+  mqttClient.poll();  // keep connection to broker alive
+
   uint16_t capread = ss.touchRead(0);
 
   if (count == capLen) {
     uint16_t avg = capAvg(capVals);
     float tempC = ss.getTemp();
     Serial.print("Temperature: "); Serial.print(tempC); Serial.println("*C");
-    Serial.print("Capacitive: "); Serial.println(capread);
-    Serial.print("Avgerage: "); Serial.println(avg);
-    //      sendMessage(avg, tempC);
+    Serial.print("Avgerage Capactitance: "); Serial.println(avg);
+    sendMessage(avg, tempC);
     count = 0;
   }
   capVals[count] = capread;
 
-  delay(500);
+  delay(loopDelay);
   count += 1;
-  Serial.println(count);
 }
